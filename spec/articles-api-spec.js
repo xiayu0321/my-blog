3c7618030be7e098703cef989981a9e6db5e7480
Peng.Li <nowind_lee@qq.com>
2016-08-16 11:59:29 +0800

    spec complete

--- spec/articles-api-spec.js
+++ spec/articles-api-spec.js
@@ -2,12 +2,26 @@
 import request from 'supertest';
 import app from '../app/server';
 import finish from './finish';
+import {Article} from '../app/db/schema';
 
 describe('articles-api', () => {
+  beforeEach((done)=> {
+    Article.find().remove(finish(done));
+  });
+
   it('post articles', (done) => {
     request(app)
       .post('/api/articles')
       .send({title: 'my-title', content: 'my-content'})
-      .expect(201, finish(done));
+      .expect(201, function (err, data) {
+        Article.find(function (err, articles) {
+          expect(articles.length).toEqual(1);
+          const saved = articles[0];
+          expect(saved.title).toEqual('my-title');
+          expect(saved.content).toEqual('my-content');
+
+          finish(done)(err);
+        })
+      });
   });
 });
